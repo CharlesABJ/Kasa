99ed7e6be8d12902dabfcf43be6c455558bf11d8
CharlesABJ <biadjeu.charles@orange.fr>
2023-04-25 18:55:44 +0200

    Design du Header, Footer et de la page error404 sont indentiques au design, les élements sont fonctionnels

--- src/pages/Error404.js
+++ src/pages/Error404.js
@@ -1,15 +1,18 @@
 import React from "react";
 import Header from "../components/Header";
 import "../styles/pages/Error404.css";
+import { NavLink } from "react-router-dom";
 
 function Error404() {
   return (
     <div>
       <Header />
-      <main className="error-page">
-        <h1>Erreur 404</h1>
+      <main className="error404">
+        <h1>404</h1>
         <p>Oups! La page que vous demandez n'existe pas.</p>
-        <a href="/">Retourner sur la page d’accueil</a>
+        <NavLink to="/">
+          <span>Retourner sur la page d’accueil</span>
+        </NavLink>
       </main>
     </div>
   );
