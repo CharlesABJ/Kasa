9fc87cd61771cfc60ae431052c7501baeea74034
CharlesABJ <biadjeu.charles@orange.fr>
2023-04-25 19:38:34 +0200

    Création et mise en place d'un composant pour la banniere de la page home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+import Saucisse from "../components/Saucisse";
 import "../styles/pages/Error404.css";
 
 function Home() {
   return (
     <div>
       <Header />
-      <h1>Chez vous, partout et ailleurs</h1>
+      <Saucisse />
       <Footer />
     </div>
   );
