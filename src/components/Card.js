9f4aa59f055ecfc947576301317c10a83b162225
CharlesABJ <biadjeu.charles@orange.fr>
2023-04-26 02:41:33 +0200

    Mise en place du hover pour l'apparition des images en CSS putot qu'en React

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,23 +1,8 @@
-import React, { useState } from "react";
 import "../styles/components/Card.css";
 function Card(props) {
-  const [isHovered, setIsHovered] = useState(false);
-
-  const handleMouseEnter = () => {
-    setIsHovered(true);
-  };
-  const handleMouseLeave = () => {
-    setIsHovered(false);
-  };
   return (
     <div className="card">
-      <img
-        className={isHovered ? "card-hovered " : ""}
-        onMouseEnter={handleMouseEnter}
-        onMouseLeave={handleMouseLeave}
-        src={props.cover}
-        alt="logement"
-      />
+      <img src={props.cover} alt="logement" />
       <span>{props.title}</span>
     </div>
   );
